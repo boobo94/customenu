26a8b25c21f94803945b2757ad24a0fcf2c272d7
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-11 23:13:55 +0200

    Add adapter for product

--- src/routers/web/v1/products/get.js
+++ src/routers/web/v1/products/get.js
@@ -2,6 +2,23 @@ import { Controllers } from '../../../../database';
 import errors from '../../../../locales/errors.json';
 import statusCodes from '../../../utils/statusCodes';
 
+const adapter = (element) => {
+  const response = {
+    id: element.id,
+    restaurantId: element.restaurantId,
+    image: element.image,
+    weight: element.weight,
+    price: element.price,
+  };
+
+  if (element.product_i18ns) {
+    response.name = element.product_i18ns[0].name;
+    response.description = element.product_i18ns[0].description;
+  }
+
+  return response;
+};
+
 export default async (req, res) => {
   try {
     const subcategory = await Controllers.subcategory.findByIdWithCategoryAndRestaurant(req.params.subcategoryId, req.params.categoryId, req.headers['accept-language']);
@@ -15,7 +32,7 @@ export default async (req, res) => {
       return res.status(statusCodes.NOT_FOUND).send({ error: errors.RESOURCE_NOT_FOUND });
     }
 
-    return res.status(statusCodes.OK).send(product);
+    return res.status(statusCodes.OK).send(adapter(product));
   } catch (error) {
     return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
   }
