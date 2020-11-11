a243f3c4285cec4eeb1f0b64a89ef1088ce7ca5d
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-11 23:09:40 +0200

    Add adapter in products api

--- src/routers/web/v1/products/get-all.js
+++ src/routers/web/v1/products/get-all.js
@@ -2,6 +2,23 @@ import { Controllers } from '../../../../database';
 import errors from '../../../../locales/errors.json';
 import statusCodes from '../../../utils/statusCodes';
 
+const adapter = (categories) => categories.map((element) => {
+  const response = {
+    id: element.id,
+    restaurantId: element.restaurantId,
+    image: element.image,
+    weight: element.weight,
+    price: element.price,
+  };
+
+  if (element.product_i18ns) {
+    response.name = element.product_i18ns[0].name;
+    response.description = element.product_i18ns[0].description;
+  }
+
+  return response;
+});
+
 export default async (req, res) => {
   try {
     const subcategory = await Controllers.subcategory.findByIdWithCategoryAndRestaurant(req.params.subcategoryId, req.params.categoryId, req.headers['accept-language']);
@@ -12,7 +29,7 @@ export default async (req, res) => {
 
     const products = await Controllers.product.findAll(req.params.subcategoryId, req.headers['accept-language']);
 
-    return res.status(statusCodes.OK).send(products);
+    return res.status(statusCodes.OK).send(adapter(products));
   } catch (error) {
     return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
   }
