3e7ed39696e4fac89d69ee79f0efb58a52f18a8a
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-11 19:24:28 +0200

    Create products view

--- src/components/ProductInList.vue
+++ src/components/ProductInList.vue
@@ -1,5 +1,21 @@
 <template>
-  <p>This is my product in list {{name}}</p>
+  <v-card class="mx-auto" outlined>
+    <v-list-item three-line>
+      <v-list-item-avatar tile size="100" color="grey">
+        <v-img :src="image"></v-img>
+      </v-list-item-avatar>
+
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">
+          {{ name }}
+        </v-list-item-title>
+        <div class="overline">
+          <span><v-icon >mdi-cash-multiple</v-icon> {{ price }}RON </span>
+           <span><v-icon >mdi-weight-gram</v-icon> {{ weight }}g</span>
+        </div>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
 </template>
 
 <script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <p>List all products</p>
-
-    <ProductInList v-bind="product" />
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+        v-for="element in elements"
+        :key="element.id"
+      >
+        <ProductInList v-bind="element" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -16,9 +25,40 @@ export default {
   },
   data() {
     return {
-      product: {
-        name: 'food',
-      },
+      elements: [
+        {
+          id: 1,
+          name: 'product 1',
+          image: 'https://estonianworld.com/wp-content/uploads/2019/09/burger-king.jpg',
+          description: 'lorem ipsum',
+          price: 2.3,
+          weight: 23.45,
+        },
+        {
+          id: 2,
+          name: 'product 2',
+          image: 'https://www.citrusbits.com/wp-content/uploads/2017/04/burgerking-app-development-company.jpg',
+          description: 'lorem ipsum',
+          price: 2.3,
+          weight: 23.45,
+        },
+        {
+          id: 3,
+          name: 'product 3',
+          image: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+          description: 'lorem ipsum',
+          price: 2.3,
+          weight: 23.45,
+        },
+        {
+          id: 4,
+          name: 'product 4',
+          image: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+          description: 'lorem ipsum',
+          price: 2.3,
+          weight: 23.45,
+        },
+      ],
     };
   },
 };
