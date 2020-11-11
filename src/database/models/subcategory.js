1d45737a4ae99ca2a621a9c83e42ae4c5e2d126d
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-11 22:31:51 +0200

    Add image on subcategory

--- src/database/models/subcategory.js
+++ src/database/models/subcategory.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 export default (sequelize, DataTypes) => {
-  const subcategory = sequelize.define('subcategory', { }, {});
+  const subcategory = sequelize.define('subcategory', {
+    image: {
+      type: DataTypes.STRING,
+    },
+  }, {});
   subcategory.associate = (models) => {
     // associations can be defined here
     subcategory.belongsTo(models.category);
--- src/routers/web/v1/subcategories/get-all.js
+++ src/routers/web/v1/subcategories/get-all.js
@@ -2,6 +2,20 @@ import { Controllers } from '../../../../database';
 import errors from '../../../../locales/errors.json';
 import statusCodes from '../../../utils/statusCodes';
 
+const adapter = (categories) => categories.map((element) => {
+  const response = {
+    id: element.id,
+    restaurantId: element.restaurantId,
+    image: element.image,
+  };
+
+  if (element.subcategory_i18ns) {
+    response.name = element.subcategory_i18ns[0].name;
+  }
+
+  return response;
+});
+
 export default async (req, res) => {
   try {
     const category = await Controllers.category.findById(req.params.categoryId, req.headers['accept-language']);
@@ -11,7 +25,7 @@ export default async (req, res) => {
 
     const subcategories = await Controllers.subcategory.findAll(req.params.categoryId, req.headers['accept-language']);
 
-    return res.status(statusCodes.OK).send(subcategories);
+    return res.status(statusCodes.OK).send(adapter(subcategories));
   } catch (error) {
     return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
   }
