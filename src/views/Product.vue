4911bf04e055ce0d9a95781baac1e617cd8f6de7
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-11 23:14:13 +0200

    Add preloader on product load

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,41 +1,60 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col>
-        <v-img :src="product.image"></v-img>
-      </v-col>
+    <v-row align="center" justify="center">
+      <v-progress-circular
+        v-if="isLoading"
+        indeterminate
+        color="primary"
+      ></v-progress-circular>
     </v-row>
-    <v-row>
-      <v-col cols="8" md="10">
-        <h1>{{ product.name }}</h1>
-      </v-col>
-      <v-col cols="4" md="2" class="text-end">
-        <div><v-icon>mdi-cash-multiple</v-icon> {{ product.price }}RON</div>
-        <div><v-icon>mdi-weight-gram</v-icon> {{ product.weight }}g</div>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>{{ product.description }}</v-col>
-    </v-row>
+
+    <template v-if="!isLoading">
+      <v-row>
+        <v-col>
+          <v-img :src="product.image"></v-img>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="8" md="10">
+          <h1>{{ product.name }}</h1>
+        </v-col>
+        <v-col cols="4" md="2" class="text-end">
+          <div><v-icon>mdi-cash-multiple</v-icon> {{ product.price }}RON</div>
+          <div><v-icon>mdi-weight-gram</v-icon> {{ product.weight }}g</div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>{{ product.description }}</v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Product',
 
   data() {
     return {
-      product: {
-        id: 1,
-        name: 'product 1',
-        image:
-          'https://estonianworld.com/wp-content/uploads/2019/09/burger-king.jpg',
-        description: 'lorem ipsum',
-        price: 2.3,
-        weight: 23.45,
-      },
+      isLoading: true,
+      product: null,
     };
+  },
+  async created() {
+    const {
+      restaurantUrl,
+      categoryId,
+      subcategoryId,
+      productId,
+    } = this.$route.params;
+    const { data } = await axios.get(
+      `/web/v1/${restaurantUrl}/categories/${categoryId}/subcategories/${subcategoryId}/products/${productId}`,
+    );
+
+    this.isLoading = false;
+    this.product = data;
   },
 };
 </script>
