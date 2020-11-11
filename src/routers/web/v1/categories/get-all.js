41b08a49d6e29bc042b554e7a0b5c35d9181bf34
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-11 21:23:43 +0200

    Add adapter for all categories

--- src/routers/web/v1/categories/get-all.js
+++ src/routers/web/v1/categories/get-all.js
@@ -2,11 +2,25 @@ import { Controllers } from '../../../../database';
 import errors from '../../../../locales/errors.json';
 import statusCodes from '../../../utils/statusCodes';
 
+const adapter = (categories) => categories.map((element) => {
+  const response = {
+    id: element.id,
+    restaurantId: element.restaurantId,
+
+  };
+
+  if (element.category_i18ns) {
+    response.name = element.category_i18ns[0].name;
+  }
+
+  return response;
+});
+
 export default async (req, res) => {
   try {
     const categories = await Controllers.category.findAll(req.params.restaurantId, req.headers['accept-language']);
 
-    return res.status(statusCodes.OK).send(categories);
+    return res.status(statusCodes.OK).send(adapter(categories));
   } catch (error) {
     return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
   }
