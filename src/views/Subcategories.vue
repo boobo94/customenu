f8a2865b36a356220e40205f887b6b2918cbfd9c
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-11 22:43:00 +0200

    Get subcategories from api

--- src/views/Subcategories.vue
+++ src/views/Subcategories.vue
@@ -1,7 +1,22 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="6" sm="6" md="3" lg="2" v-for="element in elements" :key="element.id">
+    <v-row align="center" justify="center">
+      <v-progress-circular
+        v-if="isLoading"
+        indeterminate
+        color="primary"
+      ></v-progress-circular>
+    </v-row>
+
+    <v-row v-if="!isLoading">
+      <v-col
+        cols="6"
+        sm="6"
+        md="3"
+        lg="2"
+        v-for="element in elements"
+        :key="element.id"
+      >
         <SquareGridElement v-bind="element" />
       </v-col>
     </v-row>
@@ -10,6 +25,7 @@
 
 <script>
 import SquareGridElement from '@/components/SquareGridElement.vue';
+import axios from 'axios';
 
 export default {
   name: 'Subcategories',
@@ -18,29 +34,19 @@ export default {
   },
   data() {
     return {
-      elements: [
-        {
-          id: 1,
-          name: 'chicken',
-          image: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
-        },
-        {
-          id: 2,
-          name: 'beef',
-          image: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
-        },
-        {
-          id: 3,
-          name: 'burgers',
-          image: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
-        },
-        {
-          id: 4,
-          name: 'pastas',
-          image: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
-        },
-      ],
+      isLoading: true,
+      elements: [],
     };
+  },
+
+  async created() {
+    const { restaurantUrl, categoryId } = this.$route.params;
+    const { data } = await axios.get(
+      `/web/v1/${restaurantUrl}/categories/${categoryId}/subcategories`,
+    );
+
+    this.isLoading = false;
+    this.elements = data;
   },
 };
 </script>
