9ad906b3b9f3d463facf43f03759aa2c9107d78f
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-12 19:45:57 +0200

    Add adapter  for restarurant api

--- src/routers/web/v1/restaurant/get.js
+++ src/routers/web/v1/restaurant/get.js
@@ -2,6 +2,28 @@ import { Controllers } from '../../../../database';
 import errors from '../../../../locales/errors.json';
 import statusCodes from '../../../utils/statusCodes';
 
+const adapter = (element) => {
+  const response = {
+    id: element.id,
+    shortUrl: element.shortUrl,
+    currency: element.currency,
+    email: element.email,
+    phone: element.phone,
+    address: element.address,
+    instagramUrl: element.instagramUrl,
+    facebookUrl: element.facebookUrl,
+    youtubeUrl: element.youtubeUrl,
+  };
+
+  if (element.restaurant_i18ns) {
+    response.name = element.restaurant_i18ns[0].name;
+    response.description = element.restaurant_i18ns[0].description;
+    response.allergens = element.restaurant_i18ns[0].allergens;
+  }
+
+  return response;
+};
+
 export default async (req, res) => {
   try {
     const restaurant = await Controllers.restaurant.findById(req.params.restaurantId, req.headers['accept-language']);
@@ -11,7 +33,7 @@ export default async (req, res) => {
       return res.status(statusCodes.FORBIDDEN).send({ error: errors.FORBIDDEN });
     }
 
-    return res.status(statusCodes.OK).send(restaurant);
+    return res.status(statusCodes.OK).send(adapter(restaurant));
   } catch (error) {
     return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
   }
