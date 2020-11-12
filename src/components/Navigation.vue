f112321e0447666c7fba71dc5e030bcf90e9de6a
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-12 19:47:20 +0200

    Set logo on navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,33 +1,61 @@
 <template>
   <v-app-bar app color="primary" dark>
-
-    <div @click="goBack" v-if="!isHomePage()">
-      <v-btn icon>
+    <div class="empty-button-size">
+      <v-btn icon @click="goBack" v-if="!isHomePage()">
         <v-icon>mdi-arrow-left</v-icon>
       </v-btn>
     </div>
 
     <v-spacer></v-spacer>
 
-    <div>Meniul Digital</div>
+    <router-link :to="{ name: 'Home' }">
+      <img :src="restaurant.logo" :alt="restaurant.shortUrl" height="50" />
+    </router-link>
 
     <v-spacer></v-spacer>
 
-    <div class="empty-button-size"></div>
+    <router-link :to="{ name: 'Contact' }">
+      <v-icon>mdi-silverware-fork-knife</v-icon>
+    </router-link>
 
   </v-app-bar>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Navigation',
   props: {},
+  data() {
+    return {
+      restaurant: {
+        id: 0,
+        logo: 'Digital Menu',
+        shortUrl: '',
+      },
+    };
+  },
+  async created() {
+    const { restaurantUrl } = this.$route.params;
+    const { data } = await axios.get(`/web/v1/${restaurantUrl}/simple`);
+
+    this.restaurant = data;
+  },
   methods: {
     goBack() {
-      this.$router.back();
+      return window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/');
     },
     isHomePage() {
       return this.$route.name === 'Home';
+    },
+    gotoRestaurant() {
+      return this.$router.push({ name: 'Contact' });
+    },
+    gotoHome() {
+      return this.$router.push({ name: 'Home' });
     },
   },
 };
