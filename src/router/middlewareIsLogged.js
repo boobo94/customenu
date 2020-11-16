d401adca79ae4d2d7d5f02365ce991ea84ec6ab3
Bogdan Alexandru Militaru <bogdan.militaru@codezilla.ro>
2020-11-16 22:44:56 +0200

    Refactor middleware is loggedIn

--- src/router/middlewareIsLogged.js
+++ src/router/middlewareIsLogged.js
@@ -1,9 +1,7 @@
 import store from '../store';
 
 export default (to, from, next) => {
-  console.log('is logged', store.getters['authModule/isLoggedIn']);
   if (!store.getters['authModule/isLoggedIn']) {
-    console.log('restart');
     next({ path: '/login' });
   }
 
