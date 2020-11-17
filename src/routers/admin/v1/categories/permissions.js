fe712560a72d002c533c024d5b5334bdb6c76388
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-11-17 21:13:57 +0200

    Implement remove category

--- src/routers/admin/v1/categories/permissions.js
+++ src/routers/admin/v1/categories/permissions.js
@@ -1,12 +1,28 @@
 import statusCodes from '../../../utils/statusCodes';
 import errors from '../../../../locales/errors.json';
 import { DecodeJWT } from '../../../utils/jwt';
+import { Controllers } from '../../../../database';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function checkAccessToCategories(req, res, next) {
   try {
     const { restaurantId } = DecodeJWT(req.headers.authorization);
     if (restaurantId !== req.params.restaurantId) {
+      return res.status(statusCodes.FORBIDDEN).send({ error: errors.FORBIDDEN });
+    }
+  } catch (err) {
+    return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
+  }
+
+  return next();
+}
+
+export async function checkAccessToDeleteCategory(req, res, next) {
+  try {
+    const category = await Controllers.category.findByIdSimple(req.params.categoryId);
+    if (!category) {
+      return res.status(statusCodes.NOT_FOUND).send({ error: errors.RESOURCE_NOT_FOUND });
+    } if (category.restaurantId !== req.params.restaurantId) {
       return res.status(statusCodes.FORBIDDEN).send({ error: errors.FORBIDDEN });
     }
   } catch (err) {
---
+++ src/routers/admin/v1/categories/remove.js
@@ -0,0 +1,13 @@
+import { Controllers } from '../../../../database';
+import errors from '../../../../locales/errors.json';
+import statusCodes from '../../../utils/statusCodes';
+
+export default async (req, res) => {
+  try {
+    await Controllers.category.delete(req.params.categoryId);
+
+    return res.status(statusCodes.NO_CONTENT).send();
+  } catch (error) {
+    return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
+  }
+};
