ffdcbf3cb6d83d34f312e3be3cacdb1a87a71259
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-11-17 21:56:38 +0200

    Add validators for categories

--- src/routers/admin/v1/categories/validator.js
+++ src/routers/admin/v1/categories/validator.js
@@ -1,4 +1,4 @@
-import { param } from 'express-validator';
+import { body, param } from 'express-validator';
 import checkErrors from '../../../validation/checkErrors';
 
 export const getCategory = [
@@ -17,6 +17,44 @@ export const getCategories = [
     .isInt()
     .withMessage('is int')
     .toInt(),
+
+  checkErrors,
+];
+
+export const postValidator = [
+  param('restaurantId')
+    .not().isEmpty().withMessage('is required')
+    .isInt()
+    .withMessage('is int')
+    .toInt(),
+
+  body(['category_i18ns.*.name', 'category_i18ns.*.lang_code'])
+    .not().isEmpty().withMessage('is required')
+    .isString()
+    .withMessage('is string'),
+
+  body('image')
+    .optional()
+    .isString()
+    .withMessage('is string'),
+
+  checkErrors,
+];
+
+export const putValidator = [
+  param('restaurantId')
+    .not().isEmpty().withMessage('is required')
+    .isInt()
+    .withMessage('is int')
+    .toInt(),
+
+  body(['image'])
+    .optional()
+    .isString().withMessage('is string'),
+
+  body(['category_i18ns.*.name', 'category_i18ns.*.lang_code'])
+    .optional()
+    .isString().withMessage('is string'),
 
   checkErrors,
 ];
