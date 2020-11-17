4c65f2ab95512021bcdf69bfaff86dc4f01f8d84
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-11-17 21:06:54 +0200

    Create new category

---
+++ src/routers/admin/v1/categories/create.js
@@ -0,0 +1,17 @@
+import { Controllers } from '../../../../database';
+import errors from '../../../../locales/errors.json';
+import statusCodes from '../../../utils/statusCodes';
+
+export default async (req, res) => {
+  try {
+    const newCategory = {
+      ...req.body,
+      restaurantId: req.params.restaurantId,
+    };
+    const created = await Controllers.category.create(newCategory);
+
+    return res.status(statusCodes.OK).send(created);
+  } catch (error) {
+    return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
+  }
+};
