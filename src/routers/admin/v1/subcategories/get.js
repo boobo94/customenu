d02313b929a8ea89792a88977c7b579de5f27f89
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-11-18 22:07:57 +0200

    Get one subcategory

---
+++ src/routers/admin/v1/subcategories/get.js
@@ -0,0 +1,16 @@
+import { Controllers } from '../../../../database';
+import errors from '../../../../locales/errors.json';
+import statusCodes from '../../../utils/statusCodes';
+
+export default async (req, res) => {
+  try {
+    const subcategory = await Controllers.subcategory.findByIdWithCategoryAndRestaurant(req.params.subcategoryId, req.params.categoryId, req.headers['accept-language']);
+    if (!subcategory) {
+      return res.status(statusCodes.NOT_FOUND).send({ error: errors.RESOURCE_NOT_FOUND });
+    }
+
+    return res.status(statusCodes.OK).send(subcategory);
+  } catch (error) {
+    return res.status(statusCodes.SERVER_INTERNAL_ERROR).send({ error: errors.SERVER_ERROR });
+  }
+};
