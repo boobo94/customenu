f5d995a67a71d482f030bce3a9ef5bb7cb61a634
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-11-24 22:22:19 +0200

    Update pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,16 +17,16 @@ module.exports = {
     env: {
       NODE_ENV: 'development',
 
+      APP_PORT: 8000,
+      BCRYPT_SALT: 9,
+      JWT_SECRET_KEY: 'qKNWpC5h',
+
       // database
       DB_USERNAME: 'digital_menu_user',
       DB_PASSWORD: 'Hakj5KPs',
       DB_DATABASE: 'digital_menu_db',
       DB_HOST: '127.0.0.1',
       DB_DIALECT: 'postgres',
-
-      APP_PORT: 8000,
-      BCRYPT_SALT: 9,
-      JWT_SECRET_KEY: 'qKNWpC5h',
 
       // linode object bucket
       LINODE_OBJECT_STORAGE_ENDPOINT: 'eu-central-1.linodeobjects.com',
@@ -40,21 +40,10 @@ module.exports = {
       APP_PORT: 8005,
 
       DB_USERNAME: 'digital_menu_user',
-      DB_PASSWORD: 'LqWgzS5L',
+      DB_PASSWORD: 'eSbWNzStmLxEBk9y',
       DB_DATABASE: 'digital_menu_db',
 
-      JWT_SECRET_KEY: 'qKNWpC5h',
+      JWT_SECRET_KEY: 'kXZ7aM38Hsz43WTK',
     },
   }],
-
-  deploy: {
-    production: {
-      user: 'node',
-      host: '212.83.163.1',
-      ref: 'origin/master',
-      repo: 'git@github.com:repo.git',
-      path: '/var/www/production',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-    },
-  },
 };
