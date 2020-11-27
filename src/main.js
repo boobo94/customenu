a7c86e6b1845d461dbce146728a35015e55ae9de
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-11-27 21:52:18 +0200

    Set default base url for api

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import i18n from './i18n';
 import store from './store';
 
 // setup axios
-axios.defaults.baseURL = 'http://127.0.0.1:8000/admin/v1';
+
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.customenu.ro' : 'http://127.0.0.1:8000';
+axios.defaults.baseURL = `${API_BASE_URL}/admin/v1`;
 axios.defaults.headers['accept-language'] = i18n.locale || 'en';
 axios.defaults.headers.authorization = localStorage.getItem('auth_token')
   ? `Bearer ${localStorage.getItem('auth_token')}`
