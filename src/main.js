5bdfab889248eb860a6264d4fdb9af98bf59c0cb
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-11-27 22:45:09 +0200

    Set default base url

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import vuetify from './plugins/vuetify';
 import router from './router';
 import i18n from './i18n';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.customenu.ro' : 'http://127.0.0.1:8000';
+axios.defaults.baseURL = API_BASE_URL;
 axios.defaults.headers['accept-language'] = i18n.locale || 'en';
 
 new Vue({
