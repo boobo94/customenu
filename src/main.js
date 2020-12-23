fa7f96bc312882b6eecfdb9cc4cb71a0fdd55ecd
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-12-23 17:50:55 +0200

    Fix environment on prod

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from './router';
 import i18n from './i18n';
 import store from './store';
 
-const API_BASE_URL = process.env.NODE_ENV !== 'production' ? 'https://api.customenu.ro' : 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://api.customenu.ro' : 'http://127.0.0.1:8000';
 axios.defaults.baseURL = API_BASE_URL;
 axios.defaults.headers['accept-language'] = i18n.locale || 'en';
 
