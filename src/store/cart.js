6de9ee17d84ce266a642243f2dca35d429b020bc
boobo94 <militaru.bogdanalexandru@gmail.com>
2020-12-23 18:34:01 +0200

    Decrease qunatity from cart if more than 1 pic

--- src/store/cart.js
+++ src/store/cart.js
@@ -7,8 +7,11 @@ export default {
     add(state, product) {
       state.products.push(product);
     },
-    updateQuantity(state, index) {
+    increaseQuantity(state, index) {
       state.products[index].quantity++;
+    },
+    decreaseQuantity(state, index) {
+      state.products[index].quantity--;
     },
     delete(state, id) {
       state.products = state.products.filter((product) => product.id !== id);
@@ -25,13 +28,21 @@ export default {
           ...product,
           quantity: 1,
         });
-      } else { // update only quantyty
-        commit('updateQuantity', productPosition);
+      } else { // update only quantity
+        commit('increaseQuantity', productPosition);
       }
     },
 
-    delete({ commit }, id) {
-      commit('delete', id);
+    delete({ commit, state }, id) {
+      const productPosition = state.products.findIndex(
+        (element) => element.id === id && element.quantity > 1,
+      );
+
+      if (productPosition === -1) { // not found, add it
+        commit('delete', id);
+      } else { // update only quantity
+        commit('decreaseQuantity', productPosition);
+      }
     },
 
     reset({ commit }) {
