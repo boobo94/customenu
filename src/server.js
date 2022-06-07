5d1961e7bcb92c7fa6ef1aa04f01673987541120
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-06-07 21:21:46 +0300

    Fix some orchestration

--- src/server.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/first */
-require('dotenv').config({ path: '.env' });
-
-import app from './app';
-
-app.listen(process.env.APP_PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Express server listening on port ${process.env.APP_PORT}`);
-});
