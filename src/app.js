13e873d474bc8c59d43ee1cfc7d2b2664e33cee9
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-06-19 23:22:46 +0300

    Add i18n to the app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import i18n from 'i18n';
+import { join } from 'path';
 import router from './routers';
 
 import { InitDB } from './database';
@@ -12,6 +14,14 @@ app.use('/webhooks', webhooks);
 app.use(cors());
 app.use(morgan('common'));
 app.use(express.json({ limit: '10mb' }));
+
+i18n.configure({
+  locales: ['en', 'ro'],
+  defaultLocale: 'en',
+  directory: join(__dirname, 'locales'),
+  header: 'accept-language',
+});
+app.use(i18n.init);
 
 // initialize database
 InitDB();
