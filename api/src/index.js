77c50398f8ff6e6aca62596b112f97a0ba298b15
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-06-24 13:45:23 +0300

    Change API_PORT

--- api/src/index.js
+++ api/src/index.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: `${__dirname}/../../.env` });
 
 import app from './app';
 
-app.listen(process.env.APP_PORT, () => {
+app.listen(process.env.API_PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Express server listening on port ${process.env.APP_PORT}`);
+  console.log(`Express server listening on port ${process.env.API_PORT}`);
 });
