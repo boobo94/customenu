963ee11d41b7442948ecf92571dae7d100805eb5
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-06-24 15:54:59 +0300

    Test docker cache

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,7 @@
 require('dotenv').config({ path: `${__dirname}/../../.env` });
 
 import app from './app';
+// todo: test docker cache
 
 app.listen(process.env.API_PORT, () => {
   // eslint-disable-next-line no-console
