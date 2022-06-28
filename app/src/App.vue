04537a3524ec86653d16744ccf53265455f94853
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-06-28 18:20:01 +0300

    Create progress loader component

--- app/src/App.vue
+++ app/src/App.vue
@@ -16,27 +16,6 @@ import BottomBar from './components/BottomBar.vue';
 <style lang="scss">
 @import "@/styles/colors.scss";
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
 .v-main {
   background-color: $background-color;
 }
---
+++ app/src/components/ProgressLoader.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-progress-circular
+      v-if="value"
+      class="screen-center"
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
+
+    <h1 v-if="message != null">{{ message }}</h1>
+  </v-container>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  value: {
+    type: Boolean,
+    default: false,
+  },
+  message: {
+    type: String,
+  },
+});
+</script>
+
+<style>
+.screen-center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+}
+</style>
