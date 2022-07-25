aa55b9b14d14633529df6cae69c82aaf3170aae5
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-07-25 19:14:17 +0300

    Add logo on the qrcode #30

--- api/src/routers/admin/v1/tables/download-qr-code.js
+++ api/src/routers/admin/v1/tables/download-qr-code.js
@@ -1,8 +1,29 @@
 import * as qrcode from 'qrcode';
+import { createCanvas, loadImage } from 'canvas';
 import { getMinimal } from '../../../../database/services/restaurant';
 import { findOne } from '../../../../database/services/table';
 import errors from '../../../../locales/errors.json';
 import statusCodes from '../../../utils/statusCodes';
+
+async function generateQR(restaurantUrl, tableName) {
+  const qrCodeCanvas = createCanvas(1, 1);
+  const ctx = qrCodeCanvas.getContext('2d');
+
+  // add the qr code to canvas
+  await qrcode.toCanvas(qrCodeCanvas, `${process.env.APP_URL}${restaurantUrl}?table=${tableName}`, {
+    errorCorrectionLevel: 'H',
+    width: 300,
+  });
+
+  const logo = await loadImage('./public/images/logo.png');
+  const logoSize = 128;
+  const xCenter = (qrCodeCanvas.width - logoSize) / 2;
+  const yCenter = (qrCodeCanvas.height - logoSize) / 2;
+
+  ctx.drawImage(logo, xCenter, yCenter, logoSize, logoSize);
+
+  return qrCodeCanvas.toBuffer();
+}
 
 export default async (req, res) => {
   try {
@@ -12,10 +33,7 @@ export default async (req, res) => {
       return res.status(statusCodes.NOT_FOUND).send({ error: errors.RESOURCE_NOT_FOUND });
     }
 
-    const qrCodeFile = await qrcode.toBuffer(`${process.env.APP_URL}${restaurant.shortUrl}?table=${table.name}`, {
-      errorCorrectionLevel: 'H',
-      width: 300,
-    });
+    const qrCodeFile = await generateQR(restaurant.shortUrl, table.name);
 
     return res
       .set({
