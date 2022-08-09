ec59588f307872adcbb4e98f0be513d26b05d4aa
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-08-09 17:16:15 +0300

    Fix docker command

--- bin/force-rebuild-docker.sh
+++ bin/force-rebuild-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose --env-file .env -p customenu -f docker/docker-compose.yml -f docker/docker-compose.prod.yml --build --no-cache
+docker-compose --env-file .env -p customenu -f docker/docker-compose.yml -f docker/docker-compose.prod.yml build --no-cache
 docker-compose --env-file .env -p customenu -f docker/docker-compose.yml -f docker/docker-compose.prod.yml up --remove-orphans
