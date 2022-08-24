af80168c60330e439afb49605a7bbbe354765ac7
boobo94 <militaru.bogdanalexandru@gmail.com>
2022-08-24 17:49:53 +0300

    Run docker system prune without confirmation

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@
 git pull
 
 # remove all stopped containers, networks not used by at least one container, dangling images, dangling build cache
-docker system prune -y
+docker system prune -f
 
 # build docker
 docker-compose --env-file .env -p customenu -f docker/docker-compose.yml -f docker/docker-compose.prod.yml up --build -d --remove-orphans
